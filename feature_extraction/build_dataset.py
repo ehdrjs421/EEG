d0926a07d6e06ddf9b6b061d5ffa04b838ec75c3
ehdrjs421 <ehdrjs421@gmail.com>
2025-12-24 16:00:11 +0900

    "[modify] 데이터 추가 학습"

--- feature_extraction/build_dataset.py
+++ feature_extraction/build_dataset.py
@@ -79,7 +79,7 @@ def build_dataset(
         patient_id = os.path.basename(patient_dir)
         # if patient_id == 'chb05': ############ 나중에 삭제
         #     break
-        if not ('chb01' <= patient_id <= 'chb04'):
+        if not ('chb09' <= patient_id <= 'chb12'):
             continue
         # # 데이터 처리 분리 진행
         print(f"{patient_id}")
